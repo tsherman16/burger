0bcf0fec651b83a23678b518fa46e621a94be2a5
Tim Sherman <tim.rs.sherman@gmail.com>
2019-08-24 14:15:19 -0700

    info showing, functionality not working

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Create the database day_planner_db and specified it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table plans.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Chicken Sandwich");
